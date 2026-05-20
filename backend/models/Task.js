fabbff7a8bf0c2fdefc3e7a5067c35c5beb16422
nanduholkar <nanduholkar7@gmail.com>
2026-05-20 23:49:27 +0530

    Add Task model with schema definition for task management

---
+++ backend/models/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const todoSchema = new mongoose.Schema({
+    text: {type: String, required: true},
+    completed: {type: Boolean, default: false},
+})
+
+const taskSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, default: ""},
+    priority: {type: String, enum: ["low", "medium", "high"], default: "medium"},
+    status: {type: String, enum: ["pending","In progress","completed" ], default: "pending"},
+    dueDate: {type: Date, required: true},
+    assignedTo: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    createdBy: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}], 
+    attachment: [{type: String}],
+    todoChecklist: [todoSchema],
+    Progress: {type: Number, default: 0}
+})
+
+module.exports = mongoose.model("Task", taskSchema)
