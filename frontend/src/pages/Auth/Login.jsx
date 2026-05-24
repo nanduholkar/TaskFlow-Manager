fffe3c39df8a7df1df841d8b13378df5ecda6915
nanduholkar <nanduholkar7@gmail.com>
2026-05-24 16:30:15 +0530

    Implement login functionality with form validation and API integration

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,10 +1,108 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import AuthLayout from '../../components/layout/AuthLayout'
+import {useNavigate, Link} from "react-router-dom"
+import Input from '../../components/Inputs/Input.jsx'
+import { validateEmail } from "../../utils/helper.js"
+import { API_PATHS } from "../../utils/apiPaths.js";
+import axiosInstance  from "../../utils/axiosInstance.js"
+import axios from 'axios'
+import { UserContext } from '../../context/UserContext.jsx'
 
-function Login() {
+
+
+const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
+
+  const {updateUser} = useContext(UserContext)
+
+  const navigate = useNavigate();
+
+  // Handel Login Form submit
+  const handelLogin = async (e) => {
+    e.preventDefault()
+
+    if(!validateEmail(email)){
+      setError("Please Enter a valid email address")
+      return
+    }
+    if(!password){
+      setError("Please enter the Password")
+      return
+    }
+
+    setError("")
+    try {
+      const response = await axiosInstance.post(
+        API_PATHS.AUTH.LOGIN,
+        {
+          email,
+          password,
+        }
+      );
+    
+      const { token, role } = response.data;
+    
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(response.data)
+    
+        if (role === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/user/dashboard");
+        }
+      }
+    
+    } catch (error) {
+    
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again");
+      }
+    
+    }
+  }
+
+  // Login Api Call
+
+
   return (
-    <div>
-      Login
-    </div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center   ' >
+        <h3 className='text-xl font-semibold text-black' >Welcome Back</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6  ' >Please Enter the details</p>
+
+        <form onSubmit={handelLogin}>
+          <Input 
+            value={email}
+            onChange={(value) => setEmail(value)}
+            label= "Email Address"
+            placeholder='john@example.com'
+            type='text'
+          />
+
+          <Input 
+            value={password}
+            onChange={(value) => setPassword(value)}
+            label= "Password"
+            placeholder='Min 8 characters'
+            type='password'
+          />
+
+          {error && <p className='text-red-500 text-xs pb-2.5' >{error}</p> }
+
+          <button type='submit' className='btn-primary' >LOGIN</button>
+          <p className='text-[13px] text-slate-800 mt-3 ' >
+            Don't have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signup" >SignUp</Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
   )
 }
 
