918a7d1a68db45bb7d7c1f55d767aab5c46a3539
nanduholkar <nanduholkar7@gmail.com>
2026-05-28 11:11:20 +0530

    Update header title in Navbar component to 'TaskFlow-Manager'

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = ({activeMenu}) => {
                     <HiOutlineMenu className="text-2xl"/>
                 )}
             </button>
-            <h2 className="text-lg font-medium text-black" >Task Manager</h2>
+            <h2 className="text-lg font-medium text-black" >TaskFlow-Manager</h2>
             {openSideMenu &&(
                 <div className="fixed top-[61px] -ml-4" >
                     <SideMenu activeMenu={activeMenu}/>
