aa3f1c7ac73f8d8a1f434775c92958c11bffd30d
nanduholkar <nanduholkar7@gmail.com>
2026-05-28 11:11:32 +0530

    Implement CreateTask component with form handling and layout

--- frontend/src/pages/Admin/CreateTask.jsx
+++ frontend/src/pages/Admin/CreateTask.jsx
@@ -1,10 +1,137 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DashboardLayout from "../../components/layout/DashboardLayout.jsx";
+import {PRIORITY_DATA} from "../../utils/data.js"
+import axiosInstance from '../../utils/axiosInstance.js';
+import {API_PATHS} from '../../utils/apiPaths.js';
+import toast from "react-hot-toast"
+import { useLocation, useNavigate } from 'react-router-dom';
+import moment from "moment"
+import { LuTrash2 } from 'react-icons/lu';
+import SelectDropDown from '../../components/Inputs/SelectDropDown.jsx';
 
-function CreateTask() {
+
+
+const CreateTask = () => {
+
+  const location = useLocation();
+  const {taskId} = location.state || {}
+  const navigate = useNavigate()
+
+  const [taskData, setTaskData] = useState({
+    title: "",
+    description: "", 
+    priority: 'Low',
+    dueDate: null,
+    assignedTo: [],
+    todoChecklist: [],
+    attachment: []
+  })
+
+  const [currentTask, setCurrentTask] = useState("")
+
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [openDeleteAlert, setOpenDeleteAlert] = useState(false)
+
+  const handleValueChange = (key, value) => {
+    setTaskData((prevData) => ({...prevData, [key]: value}))
+  }
+
+  const clearData = () =>{
+    // reset form 
+    setTaskData({
+      title: '',
+      description: "",
+      priority: "Low",
+      dueDate: null,
+      assignedTo: [], 
+      todoChecklist: [],
+      attachment: []
+    })
+  }
+
+  // Create Task 
+  const createTask = async () => {
+
+  }
+
+  const updateTask = async () => {
+
+  }
+
+  const handleSubmit = async () => {
+
+  }
+
+  const getTaskDetailsById = async () => {
+    
+  }
+  
+
+
   return (
-    <div>
-      CreateTask
-    </div>
+    <DashboardLayout activeMenu="Create Task">
+      <div className='mt-5'>
+        <div className='grid grid-cols-1 md:grid-cols-4 mt-4' >
+          <div className='form-card col-span-3'>
+
+            <div className='flex items-center justify-between'>
+              <h2  className='text-xl md:text-xl font-medium'>
+                {taskId ? "Update Task" : "Create Task"}
+              </h2>
+              {taskId && (
+                <button 
+                  className='flex items-center gap-1.5 text-[13px] font-medium text-rose-500 bg-rose-50 rounded px-2 py-1 border border-rose-100 hover:border-rose-300 cursor-pointer'
+                  onClick={() => setOpenDeleteAlert(true)}
+                >
+                  <LuTrash2 className="text-base"/> Delete
+                </button>
+              )}
+            </div>
+
+            <div  className='mt-4'>
+              <label className="text-xs font-medium text-slate-600">Task Title</label>
+              <input
+                placeholder='Create App UI'
+                className='form-input'
+                value={taskData.title}
+                onChange={({target}) => 
+                  handleValueChange("title", target.value)
+                }
+              />
+            </div>
+
+            <div className='mt-3'>
+              <label className="text-xs font-medium text-slate-600">Description</label>
+              <textarea 
+                placeholder='Describe Task'
+                className='form-input'      
+                rows={4} 
+                value={taskData.description}
+                onChange={({target}) => 
+                handleValueChange("description", target.value)}
+                  
+              />
+            </div>
+
+            <div className='grid grid-cols-12 gap-4 mt-2'>
+              <div className='col-span-6 md:col-span-4'>
+                <label className="text-xs font-medium text-slate-600">Priority</label>   
+                <SelectDropDown
+                  options={PRIORITY_DATA}
+                  value={taskData.priority}
+                  onChange={(value) => handleValueChange("priority", value)}
+                  placeholder="Select Priority"
+                />
+              </div>
+            </div>
+
+        
+
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
